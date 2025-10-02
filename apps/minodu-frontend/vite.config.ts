0cc9c701deae870e5e9f80a532c9bf6faaef702d
Lutz <lutz@lu-re.de>
2025-10-02 22:32:44 +0200

    mend

--- apps/minodu-frontend/vite.config.ts
+++ apps/minodu-frontend/vite.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
   plugins: [sveltekit()],
   server: {
     proxy: {
+      '/api/services': {
+        target: 'http://localhost:3002',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
       '/api/forum': {
         target: 'http://localhost:3003',
         changeOrigin: true,
