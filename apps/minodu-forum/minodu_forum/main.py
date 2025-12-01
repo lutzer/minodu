50d660361ff67050db25e71aa1a0ef38fce463a2
Lutz <lutz@lu-re.de>
2025-12-01 12:34:01 +0100

    fixes main.py

--- apps/minodu-forum/minodu_forum/main.py
+++ apps/minodu-forum/minodu_forum/main.py
@@ -2,8 +2,9 @@ import logging
 
 import uvicorn
 from src.config import Config
+from src.app import app
 
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=Config().port, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=Config().port, reload=True)
