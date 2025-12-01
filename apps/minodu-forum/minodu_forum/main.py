a6a6c55d8b782e455f9484f6a96cbc8625522dcc
Lutz <lutz@lu-re.de>
2025-12-01 12:47:10 +0100

    fixes start point

--- apps/minodu-forum/minodu_forum/main.py
+++ apps/minodu-forum/minodu_forum/main.py
@@ -2,9 +2,9 @@ import logging
 
 import uvicorn
 from src.config import Config
-from src.app import app
+from src.app import app  # noqa: F401
 
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=Config().port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=Config().port, reload=True)
