f733e19cd4cdce8d71f3a2def90fc43247e6d42e
Lutz <lutz@lu-re.de>
2025-12-28 16:50:43 +0100

    adds start page

--- apps/minodu-frontend/src/routes/+layout.svelte
+++ apps/minodu-frontend/src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import FooterMenuButton from '$lib/components/common/FooterMenuButton.svelte';
+
+	import agricultureIcon from '$lib/assets/agriculture-icon.png';
+	import forumIcon from '$lib/assets/forum-icon.png';
+	import treeIcon from '$lib/assets/tree-icon.png';
+	import weatherIcon from '$lib/assets/weather-icon.png';
+	import marketIcon from '$lib/assets/market-icon.png';
 
 	let { children } = $props();
 </script>
@@ -8,15 +15,57 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div>
-	<h1>Mindou</h1>
-
-	<div class="content">
+<div class="app-container">
+	<div id="content">
 		{@render children?.()}
+	</div>
+	<div id="footer">
+		<div class="menu-bar">
+			<FooterMenuButton link="/agriculture" icon={agricultureIcon}/>
+			<FooterMenuButton link="/forum" icon={forumIcon}/>
+			<FooterMenuButton link="/" icon={treeIcon}/>
+			<FooterMenuButton link="/weather" icon={weatherIcon}/>
+			<FooterMenuButton link="/market" icon={marketIcon}/>
+		</div>
 	</div>
 </div>
 
 <style>
+	#footer {
+		position: fixed;
+		bottom:0;
+		left: 0;
+		right: 0;
+		height: 100px;
+	}
+
+	.menu-bar {
+		display: flex;
+		height: 100%;
+		max-width: 600px;
+		background-color: var(--bg-color-light);
+		margin: 0 auto;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		gap: var(--small-padding);
+		padding: 0 var(--small-padding);
+	}
+
+	@media screen and (min-width: 600px) {
+		.menu-bar {
+			border-radius: var(--border-radius) var(--border-radius);
+			margin-top: -20px;
+		}
+	}
+
+	:global(:root) {
+		--small-padding: 10px;
+		--bg-color-light: #C2B4B0;
+		--border-radius: 20px;
+	}
+
+
 	:global(.input-textarea) {
 		background: white;
 		border: 1px;
--- apps/minodu-frontend/src/routes/+page.svelte
+++ apps/minodu-frontend/src/routes/+page.svelte
@@ -1,6 +1,77 @@
-<h2>Home</h2>
+<script lang="ts">
+	import tileImageMobile from '$lib/assets/startscreen_background_mobile.png';
+	import tileImageTablet from '$lib/assets/startscreen_background_tablet.png';
 
-<ul>
-	<li><a href="/forum">Forum</a></li>
-	<li><a href="/bot">Chatbot</a></li>
-</ul>
+	let { children } = $props();
+</script>
+
+<style>
+	#tile-image-container {
+		position: absolute;
+		top:0;
+		bottom:0;
+		left:0;
+		right:0;
+	}
+
+	#tile-image-container img {
+		width: 100%;
+		height: 100%;
+		max-width: 1000px;
+		object-fit: cover;
+		display: none;
+		margin: 0 auto;
+	}
+
+	@media (max-aspect-ratio: 0.6) {
+		#tile-image-container .responsive-image.mobile {
+			display: block;
+		}
+	}
+
+	@media (min-aspect-ratio: 0.6) {
+		#tile-image-container .responsive-image.tablet {
+			display: block;
+		}
+	}
+
+	.grid-link-container {
+		position: absolute;
+		top:0;
+		bottom:0;
+		left:0;
+		right:0;
+		display: grid;
+		width: 100%;
+		height: 100%;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr 1fr;
+    }
+
+	.grid-link {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		opacity: 0;
+    }
+
+</style>
+
+<div>
+	<div id="tile-image-container">
+		<img 
+			src={tileImageMobile} 
+			alt="4 minodu tiles showing different apps"
+			class="responsive-image mobile"/>
+		<img 
+			src={tileImageTablet} 
+			alt="4 minodu tiles showing different apps"
+			class="responsive-image tablet"/>
+	</div>
+	<div class="grid-link-container">
+		<a href="/forum" class="grid-link">Forum</a>
+		<a href="/weather" class="grid-link">Weather</a>
+		<a href="/agriculture" class="grid-link">Agriculture</a>
+		<a href="/market" class="grid-link">Market</a>
+	</div>
+</div>
