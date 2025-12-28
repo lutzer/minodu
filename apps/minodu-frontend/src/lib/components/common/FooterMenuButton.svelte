b5882abc8f22ac8fabdf818a1d98e9b1e8c9da97
Lutz <lutz@lu-re.de>
2025-12-28 16:50:43 +0100

    adds start page

---
+++ apps/minodu-frontend/src/lib/components/common/FooterMenuButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let link : string 
+    export let icon : string
+</script>
+
+<style>
+    .footer-menu-button {
+        display: flex;
+        aspect-ratio: 1 / 1;
+        flex-shrink: 1;
+        flex-basis: 64px; /* or whatever size you want */
+        min-width: 32px;
+        min-height: 32px;
+    }
+
+    .footer-menu-button a {
+        height: 100%;
+        width: 100%;
+    }
+
+    .footer-menu-button img {
+        max-height: 100%;
+        width: 100%;
+        object-fit: contain;
+    }
+</style>
+
+<div class="footer-menu-button">
+    <a href={link}>
+        <img src={icon} alt="Icon for link to {link}"/>
+    </a>
+</div>
