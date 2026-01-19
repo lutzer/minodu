4e842cc02c12cf455740ee9daecaaf00935b5cd5
Lutz <lutz@lu-re.de>
2026-01-19 15:22:13 +0100

    adds agriculture page

---
+++ apps/minodu-frontend/src/lib/apis/backend/models/backendCategory.ts
@@ -0,0 +1,5 @@
+export interface BackendCategory {
+    id: number
+    name: string
+    image: string
+}
--- apps/minodu-frontend/src/lib/apis/backend/models/backendPost.ts
+++ apps/minodu-frontend/src/lib/apis/backend/models/backendPost.ts
@@ -1,4 +1,13 @@
+import type { BackendCategory } from "./backendCategory";
+
 export interface BackendPost {
 	id: number;
-    
+    author: string
+	category: BackendCategory
+	title: string
+	description: string
+	image: string
+	attachment: string
+	attachment_kb: string
+	attachment_pdf: string
 }
