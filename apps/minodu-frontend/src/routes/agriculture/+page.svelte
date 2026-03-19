22bfc591f987aa43aaa00b76fd49c26050ae5b5d
Lutz <lutz@lu-re.de>
2026-03-19 13:18:40 +0000

    mend

--- apps/minodu-frontend/src/routes/agriculture/+page.svelte
+++ apps/minodu-frontend/src/routes/agriculture/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import ExplainPageButton from '$lib/components/common/ExplainPageButton.svelte';
 
-	import explainPageFr from '$lib/assets/audio/s2_agricultire_fr.mp3'
+	import explainPageFr from '$lib/assets/audio/s2_agricultire_fr.mp3';
 	import explainPageKb from '$lib/assets/audio/s2_agricultire_kb.mp3';
 </script>
 
--- apps/minodu-frontend/src/routes/market/+page.svelte
+++ apps/minodu-frontend/src/routes/market/+page.svelte
@@ -2,7 +2,6 @@
 	import ExplainPageButton from '$lib/components/common/ExplainPageButton.svelte';
 	import MarketPage from '$lib/pages/MarketPage.svelte';
 
-
 	import explainPageFr from '$lib/assets/audio/s6_marche_fr.mp3';
 	import explainPageKb from '$lib/assets/audio/s6_marche_kb.mp3';
 </script>
